7fa569f373b47ef74395828f3c8c9fc5a3b28dbd
Mauricio-Projects <amgutierrezc19@hotmail.com>
2024-06-17 20:29:53 -0500

    params, models, templates

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,39 @@
 from django.http import HttpResponse
+from .models import Project, Task
+from django.shortcuts import render
 
 # Create your views here.
-def hello(request):
-    return HttpResponse('<h1>Hello Word<h1>')
+
+
+def index(request):
+    title = 'Django Course!!'
+    return render(request, 'index.html', {
+        'title': title
+    })
+
 
 def about(request):
-    return HttpResponse('<h2>about<h2>')
+    username = 'fazt'
+    return render(request, 'about.html', {
+        'username': username
+    })
+
+
+def hello(request, username):
+    return HttpResponse("<h2>Hello %s</h2>" % username)
+
+
+def projects(request):
+    # projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request, 'projects/projects.html', {
+        'projects': projects
+    })
+
+
+def tasks(request):
+    # task = Task.objects.get(title=tile)
+    tasks = Task.objects.all()
+    return render(request, 'tasks/tasks.html', {
+        'tasks': tasks
+    })
